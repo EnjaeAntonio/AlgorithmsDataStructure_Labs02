30daff5a24142818cae87a22f266484d96eb928d
Enjae Antonio <Enjaeantonio@outlook.com>
2023-01-13 10:49:53 -0600

    Second algo done

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿//Write and test the following methods in a Console Application. 
 //A program that produces an array of all of the characters that appear more than once in a string.
 //For example, the string “Programmatic Python” would result in the array ['p','r','o','a','m','t'].
-//A program returns an array of strings that are unique words found in the argument.
-//For example, the string “To be or not to be” returns ["to","be","or","not"].
+
 //A program that reverses a provided string 
 //A program that finds the longest unbroken word in a string and prints it
 //For example, the string "Tiptoe through the tulips" would print "through"
@@ -20,3 +19,21 @@ for (int i = 0; i < input.Length; i++)
     }
 }
 Console.WriteLine(result);
+
+//A program returns an array of strings that are unique words found in the argument.
+//For example, the string “To be or not to be” returns ["to","be","or","not"].
+
+string uniqueSentence = "To be or not to be";
+string[] uniqueSentSplit = uniqueSentence.Split(' ');
+
+for(int i = 0; i < uniqueSentSplit.Length; i++)
+{
+    for(int j = 0; j < uniqueSentSplit.Length; j++)
+    {
+        if (uniqueSentSplit[i] == uniqueSentSplit[j])
+        {
+            uniqueSentence = uniqueSentSplit[j];
+            Console.WriteLine(uniqueSentSplit[j]);
+        }
+    }
+}
