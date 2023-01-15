c474bcfe07029dec6e8161be337d128d7fbe38f4
Enjae Antonio <Enjaeantonio@outlook.com>
2023-01-15 14:11:52 -0600

    Created sentece, splitting the sentence, and iterating through the sentence using for loop

--- Program.cs
+++ Program.cs
@@ -2,10 +2,6 @@
 //A program that produces an array of all of the characters that appear more than once in a string.
 //For example, the string “Programmatic Python” would result in the array ['p','r','o','a','m','t'].
 
-
-//A program that finds the longest unbroken word in a string and prints it
-//For example, the string "Tiptoe through the tulips" would print "through"
-
 string input = "Programmatic Python";
 char[] result = new char[input.Length];
 for (int i = 0; i < input.Length; i++)
@@ -37,7 +33,7 @@ for(int i = 0; i < uniqueSentSplit.Length; i++)
         }
     }
 }
-
+ 
 //A program that reverses a provided string 
 
 Console.WriteLine("Please enter a word to check if it is a palindrome");
@@ -45,3 +41,31 @@ string userPalin = Console.ReadLine();
 char[] charArray = userPalin.ToCharArray();
 string joinedWords = String.Join("", charArray).ToLower();
 Array.Reverse(charArray);
+string reversedWord = new string(charArray);
+for (int j = 0; j < 1; j++)
+{
+    while (userPalin.Length < 3 || userPalin.IndexOf(" ") != -1)
+    {
+        Console.WriteLine("Please enter one word that is more than 3 letters and has no spaces  ");
+        userPalin = Console.ReadLine();
+    }
+
+    if (joinedWords == reversedWord)
+    {
+        Console.WriteLine($"Your word reversed is {reversedWord} and is also a palindrome");
+    }
+    else
+    {
+        Console.WriteLine($"Your word reversed is {reversedWord} and is not a palindrome");
+    }
+}
+
+//A program that finds the longest unbroken word in a string and prints it
+//For example, the string "Tiptoe through the tulips" would print "through"
+
+string sentence = "Tiptoe through the tulips";
+string[] sentSplit = sentence.Split(' ');
+for(int i = 0; i < sentSplit.Length; i++)
+{
+    Console.WriteLine(sentSplit[i]);
+}
